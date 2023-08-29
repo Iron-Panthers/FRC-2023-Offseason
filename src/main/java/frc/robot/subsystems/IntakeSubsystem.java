0969678aa17507ae9c0aa32c432d6a9d76912399
Derek <derekchen1815@gmail.com>
2023-08-29 16:05:25 -0700

    Added Motors

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,12 +4,22 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
 
+  private TalonFX intakeMotor;
+  private TalonFX wristMotor;
+  private ShuffleboardTab shuffleboard = Shuffleboard.getTab("Intake Subsystem");
+
   /** Creates a new IntakeSubsystem. */
-  public IntakeSubsystem() {}
+  public IntakeSubsystem() {
+    intakeMotor = new TalonFX(0);
+    wristMotor = new TalonFX(1);
+  }
 
   @Override
   public void periodic() {}
